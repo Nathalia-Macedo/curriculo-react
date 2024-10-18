82fffa38341cdc44abaf3612e4026b84cebc2d54
Nathalia-Macedo <nathaliademacedomartins04@gmail.com>
2024-10-18 10:39:18 -0300

    nao mexe mais nessa galeria mulher pelo amor de deus

--- src/Components/Galery/Galery.jsx
+++ src/Components/Galery/Galery.jsx
@@ -1,45 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import './Galery.css';
+import React from 'react';
+import './Galery.css'; // Importando o CSS da galeria
 
+// Função para importar automaticamente todas as imagens da pasta
 function importAll(r) {
   return r.keys().map(r);
 }
 
-const images = importAll(require.context('../../Assets/images', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../../Assets/images', false, /\.(png|jpe?g|svg)$/)); // Mude o caminho para a pasta onde estão suas imagens
 
 const Gallery = () => {
-  const [visibleImages, setVisibleImages] = useState(6);
-  const [fadeIn, setFadeIn] = useState(false);
-
-  const handleLoadMore = () => {
-    setFadeIn(false);
-    setVisibleImages((prev) => prev + 6);
-  };
-
-  useEffect(() => {
-    if (visibleImages < images.length) {
-      setFadeIn(true);
-    }
-  }, [visibleImages]);
-
   return (
     <div className="gallery-container">
+      {/* Texto de agradecimento */}
       <div className="thank-you-message">
         <h1>Minha Trajetória</h1>
         <p>Gostaria de agradecer a todos os meus alunos, amigos e familiares que fizeram parte da minha jornada até aqui. Cada um de vocês é parte essencial dessa caminhada. Muito obrigado!</p>
       </div>
+
+      {/* Galeria de Imagens */}
       <div className="gallery">
-        {images.slice(0, visibleImages).map((image, index) => (
-          <div className={`gallery-item ${fadeIn ? 'fade-in' : ''}`} key={index}>
+        {images.map((image, index) => (
+          <div className="gallery-item" key={index}>
             <img src={image} alt={`gallery-img-${index}`} />
           </div>
         ))}
       </div>
-      {visibleImages < images.length && (
-        <button className="load-more" onClick={handleLoadMore}>
-          Ver mais
-        </button>
-      )}
     </div>
   );
 };
